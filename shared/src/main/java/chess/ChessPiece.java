484d867140541829521097f5bf42fbf00340bb21
CallumGalloway <gallocal@byu.edu>
2025-09-19 14:06:05 -0600

    fixed bishop/rook/queen capture enemy logic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -148,6 +148,7 @@ public class ChessPiece {
                 return moves;
             }
             else{
+                moves.add(new ChessMove(myPosition, newPosition, null));
                 return moves;
             }
         }
