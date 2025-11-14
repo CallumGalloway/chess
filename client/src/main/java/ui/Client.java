7894441cce35cdacdcfcc9ab2c54432e5546a8ac
CallumGalloway <gallocal@byu.edu>
2025-11-14 16:22:11 -0700

    initial client and server facade outline

---
+++ client/src/main/java/ui/Client.java
@@ -0,0 +1,24 @@
+package ui;
+
+public class Client {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void displayWelcome(){
+
+    }
+
+    public static void displayHelp(){
+
+    }
+
+    public static void displayPostLogin(){
+
+    }
+
+    public static void repl(String[] args){
+
+    }
+}
